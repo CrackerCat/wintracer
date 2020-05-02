49e607b74051a1f1311658f496089acd0ab52ba6
Jeong Wook Oh <oh.jeongwook@gmail.com>
2020-05-01 19:08:45 -0700

    Disabled VirtualAlloc hook

--- officehooks/scripts/main.js
+++ officehooks/scripts/main.js
@@ -75,10 +75,10 @@ function hookLoadLibraryExW(moduleName) {
 var hookMap = {
     'VBE7.DLL': [hookRtcShell, hookRtcCreateObject2, hookVBAStrCat, hookVBAStrComp],
     'OLEAUT32.DLL': [hookDispCall],
-    'kernel32.dll': [hookLoadLibraryA, hookLoadLibraryExA, hookLoadLibraryW, hookLoadLibraryExW, 
-                    hookVirtualAlloc, hookCreateFileW, ], // hookReadFile
-    'kernelbase.dll': [hookLoadLibraryA, hookLoadLibraryExA, hookLoadLibraryW, hookLoadLibraryExW, 
-                    hookVirtualAlloc, hookCreateFileW, ] // hookReadFile
+    'kernel32.dll': [hookLoadLibraryA, hookLoadLibraryExA, hookLoadLibraryW, hookLoadLibraryExW, hookCreateFileW, hookReadFile
+                    ], // hookReadFile hookVirtualAlloc, hookCreateFileW, 
+    'kernelbase.dll': [hookLoadLibraryA, hookLoadLibraryExA, hookLoadLibraryW, hookLoadLibraryExW, hookCreateFileW, hookReadFile
+                     ] // hookReadFile hookVirtualAlloc, hookCreateFileW,
 }
 
 for(var dllName in hookMap) {
