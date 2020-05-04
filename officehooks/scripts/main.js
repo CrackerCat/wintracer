76aea8319341386ee649057a8046b10986e2c137
Jeong Wook Oh <oh.jeongwook@gmail.com>
2020-05-04 15:45:25 -0700

    Restyled code

--- officehooks/scripts/main.js
+++ officehooks/scripts/main.js
@@ -1,15 +1,52 @@
 var hookMap = {
-    'VBE7.DLL': [hookRtcShell, hookRtcCreateObject2, hookVBAStrCat, hookVBAStrComp],
-    'OLEAUT32.DLL': [hookDispCall],
-    'kernel32.dll': [hookLoadLibraryA, hookLoadLibraryExA, hookLoadLibraryW, hookLoadLibraryExW, hookCreateProcessW, hookCreateProcessA
-                    ], // hookReadFile hookVirtualAlloc, hookCreateFileW, 
-    'kernelbase.dll': [hookLoadLibraryA, hookLoadLibraryExA, hookLoadLibraryW, hookLoadLibraryExW, hookCreateProcessW, hookCreateProcessA
-                     ], // hookReadFile hookVirtualAlloc, hookCreateFileW,
-    'ole32': [hookOLE32Funcs, hookCoCreateInstanceEx, hookCoCreateInstance, hookCDefObjectInitFromData, 
-        hookCDefObjectLoad, hookCDefObjectRun, hookReadOleStg, hookStRead],
-    "rpcrt4": [hookI_RpcBindingCreateNP, hookRpcBindingFromStringBindingA, hookRpcBindingFromStringBindingW, hookI_RpcSend],
-    "combase": [hookCStdIdentityCreateServer, hookObjectStublessClient],
-    "coml2": [hookComl2Functions, hookCExposedStreamMethods, hookCExposedStreamRead]
+    // 'VBE7.DLL': [hookRtcShell, hookRtcCreateObject2, hookVBAStrCat, hookVBAStrComp],
+    // 'OLEAUT32.DLL': [hookDispCall],
+    'kernel32.dll': [
+        hookLoadLibraryA,
+        hookLoadLibraryExA,
+        hookLoadLibraryW,
+        hookLoadLibraryExW,
+        hookCreateProcessW,
+        hookCreateProcessA,
+        // hookReadFile,
+        // hookVirtualAlloc,
+        // hookCreateFileW,
+    ],
+    'kernelbase.dll': [
+        hookLoadLibraryA,
+        hookLoadLibraryExA,
+        hookLoadLibraryW,
+        hookLoadLibraryExW,
+        hookCreateProcessW,
+        hookCreateProcessA,
+        // hookReadFile,
+        // hookVirtualAlloc,
+        // hookCreateFileW,
+    ],
+    // 'ole32': [hookCoCreateInstanceEx, hookCoCreateInstance, hookCDefObjectInitFromData, 
+        // hookCDefObjectLoad, hookCDefObjectRun, hookReadOleStg, hookStRead], // hookOLE32Funcs
+    "rpcrt4": [
+        hookLRPC_SCALL_LRPC_SCALL, 
+        hookNdrClientInitializeNew,
+        hookRpcBindingInqObject,
+        hookI_RpcAsyncSetHandle,
+        hookI_RpcBindingCreateNP,
+        hookRpcBindingFromStringBindingA,
+        hookRpcBindingFromStringBindingW,
+        hookI_RpcSend, hookRPCFunctions,
+        hookLRPC_ADDRES_HandleRequest,
+        hookLRPC_ADDRES_AlpcSend
+    ],
+    // "combase": [hookCStdIdentityCreateServer, hookObjectStublessClient],
+    // "coml2": [hookCExposedStreamRead] // hookComl2Functions, hookCExposedStreamMethods
+    "ntdll": [
+        hookNtAlpcConnectPort,
+        hookNtSecureConnectPort,
+        hookNtAlpcConnectPortEx,
+        hookNtConnectPort,
+        hookNtAlpcAcceptConnectPort,
+        hookNtAlpcSendWaitReceivePort
+    ]
 }
 
 for(var dllName in hookMap) {
